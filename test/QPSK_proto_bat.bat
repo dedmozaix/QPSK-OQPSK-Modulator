3546b0d909bbb01296d2e289dd321b25fbada1b6
Nik <79398119+dedmozaix@users.noreply.github.com>
2024-01-12 13:40:45 +0300

    Update QPSK_proto_bat.bat

--- test/QPSK_proto_bat.bat
+++ test/QPSK_proto_bat.bat
@@ -1,22 +1,11 @@
 @echo off
 echo Start %time%
 
-::C:\Users\nik11\source\repos\Work_QPSK_OPQSK\Debug\
-::Work_QPSK_OQSK.exe 1 0
-::cd C:\liquid_test\liquid_test\bin\
+OQPSK_Modulator.exe 1 0
+
+::запуск ликвида
 ::liquid_test.exe -i Modulated.bin -o out.bin -T f -p Modulated_noNoise.bin
 
-OQPSK_Modulator.exe 1 0
-liquid_test.exe -i Modulated.bin -o out.bin -T f -p Modulated_noNoise.bin
-
-OQPSK_Modulator.exe 1 1
-liquid_test.exe -i Modulated.bin -o out.bin -T f -p Modulated_noNoise.bin
-
-OQPSK_Modulator.exe 1 2
-liquid_test.exe -i Modulated.bin -o out.bin -T f -p Modulated_noNoise.bin
-
-OQPSK_Modulator.exe 1 3
-liquid_test.exe -i Modulated.bin -o out.bin -T f -p Modulated_noNoise.bin
 
 
 echo Stop %time%
